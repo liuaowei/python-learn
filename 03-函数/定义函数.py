c8d43d2c1d29eadd1ab3ae5f4c9b7f30d4e69d28
liuaowei <liuaowei@int-yt.com>
2025-03-31 17:11:12 +0800

    提交

--- 03-函数/定义函数.py
+++ 03-函数/定义函数.py
@@ -3,5 +3,22 @@ def my_abs(x):
         return x
     else:
         return -x
+    
+# 如果想定义一个什么事也不做的函数，可以用pass语句
+def nop():
+    # 缺少了pass，代码运行就会有语法错误
+    pass
+
+# 参数检查
+def my_abs2(x):
+    if not isinstance(x,(int,float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+
 
 print(my_abs(-99))
+
+print('加上类型检查：',my_abs2('as'))
